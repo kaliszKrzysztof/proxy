98ebbfc87d0833995467201d8dce5f6c699ad143
Krzysztof Kalisz <kalisz.krzysztofl@gmail.com>
2021-07-19 08:06:00 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const morgan = require("morgan");
+const cors = require('cors')
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const app = express();
+app.use(cors());
+const PORT = 3000;
+const HOST = "localhost";
+const PRIVATE_API_URL = "https://g3czeu5yof.execute-api.us-west-1.amazonaws.com/prod/";
+const PUBLIC_API_URL = 'https://o6w9tgek4l.execute-api.us-west-1.amazonaws.com/prod/';
+
+app.use(morgan('dev'));
+
+
+app.use(
+  "/proxy-private",
+  createProxyMiddleware({
+    target: PRIVATE_API_URL,
+    changeOrigin: true,
+    pathRewrite: {
+      [`^/proxy-private`]: "",
+    },
+  })
+);
+
+app.use(
+  "/proxy-public",
+  createProxyMiddleware({
+    target: PUBLIC_API_URL,
+    changeOrigin: true,
+    pathRewrite: {
+      [`^/proxy-public`]: "",
+    },
+  })
+);
+
+app.listen(PORT, HOST, () => {
+  console.log(`Starting Proxy at ${HOST}:${PORT}`);
+});
